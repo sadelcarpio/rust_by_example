6ed91e143351169c87d546ded9611527f4a6ea46
sadelcarpio <sadelcarpioa@gmail.com>
2023-12-21 22:27:12 -0500

    String lesson

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");  // double colon for associated functions
-    server.run();  // dot for methods
+    let string = String::from("🍔🍟🍺🍺");  // cannot be sure 1 character takes 1 byte
+    // Slicing works for bytes not for each character
+    let string_slice = &string[..4];  // borrows the existing string and slices (does not take ownership)
+    let string_borrow: &str = &string;  // converts to sring slice
+    let string_literal = "1234";
+    dbg!(&string);
+    dbg!(string_slice);
+    dbg!(string_borrow);
+    dbg!(string_literal);
+    // let server = Server::new("127.0.0.1:8080");  // double colon for associated functions
+    // server.run();  // dot for methods
 }
 
 struct Server {

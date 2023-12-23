e85a644de4b92f1fda9b1fbe9dc142c64c8eddd1
sadelcarpio <sadelcarpioa@gmail.com>
2023-12-22 23:46:12 -0500

    Handling result objects

--- http_server/src/server.rs
+++ http_server/src/server.rs
@@ -1,4 +1,6 @@
 // everything inside of a module is private by default
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -11,5 +13,21 @@ impl Server {
     // method
     pub fn run(self) {  // run takes ownership if not using reference (&)
         println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();  // Result is a recoverable error Result<T, E>;
+        loop {
+            // best practice: use match for handling result
+            match listener.accept() {
+                Ok((stream, _)) => {
+
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+            // other approach: using if statement
+            let res = listener.accept();  // example of recoverable error
+            if res.is_err() {
+                continue;
+            }
+            let (stream, addr) = res.unwrap();  // here we are sure there is no error
+        }
     }
 }
